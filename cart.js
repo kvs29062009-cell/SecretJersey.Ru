6ad0e14149afcdd16a6d695fd7668e72441083ef
kvs29062009-cell <kvs29062009@gmail.com>
2026-03-10 13:29:51 +0300

    Update cart.js

--- cart.js
+++ cart.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const totalPriceEl = document.getElementById("total-price");
 
   const DELIVERY_PRICE = 550;
-  const PROMO_CODE = "UVYVUUT";
+  const PROMO_CODE = "SPRING";
   const PROMO_DISCOUNT = 0.10;
 
   // 👉 читаем промокод при загрузке
@@ -120,6 +120,7 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
 });
+
 
 
 
